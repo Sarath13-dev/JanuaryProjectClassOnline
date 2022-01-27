fd4eaa3fa72e5c10902ce6daa88e471f686ef85a
Sarath13-dev <ramsssjrs@gmail.com>
2022-01-27 09:09:38 +0530

    B

--- src/org/login/Company.java
+++ src/org/login/Company.java
@@ -7,7 +7,8 @@ import java.util.Set;
 
 public class Company {
 public static void main(String[] args) {
-	
+	System.out.println("Branch B");
+	System.out.println("My work");
 	String s ="Java Application";
 	char[] c = s.toCharArray();
 	Map<Character, Integer>m = new LinkedHashMap();
